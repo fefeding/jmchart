f217c9aea6f63de587898d4c70c9202fcceba32f
fefeding <fefeding@tencent.com>
2020-06-14 12:14:43 +0800

    tars erver

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express()
 
 app.use(express.static("."));
 
-const port = process.env.PORT || 8800;
+const port = process.env.HTTP_PORT || 8800;
+const ip = process.env.HTTP_IP || '127.0.0.1';
 
-app.listen(port)
+app.listen(port, ip);
 
 console.log(`dev server listend at ${port}`);
