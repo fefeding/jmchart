88c0aaaf4bbe7e4644bcb56ca6c405ad4200cd99
jiamao <haofefe@163.com>
2023-07-03 13:03:01 +0800

    :art: 默认不阻止滑动效果

--- src/core/markLine/manager.js
+++ src/core/markLine/manager.js
@@ -131,9 +131,9 @@ export default class jmMarkLineManager {
         }
         if(moved) {
 			if(args.longtap === 2 && args.event) {
-				args.event.preventDefault();// 阻止默认行为	
+				args.event.preventDefault();// 阻止默认行为		
+				args.event.stopPropagation();	
 			}	
-			args.event.stopPropagation();		
 
             if(!args.cancel) this.chart.emit('marklinemove', args);
         }        
